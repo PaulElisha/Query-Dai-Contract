d5ddafede80ad833ee612828a18443efe446fba9
Paul Elisha <ireajao@yahoo.com>
2023-11-27 01:08:10 +0100

    Updated file with extra data

--- DAIContract.js
+++ DAIContract.js
@@ -1,6 +1,9 @@
-const { ethers } = require("ethers");
+const { providers, Contract, utils } = require("ethers");
+require("dotenv").config();
 
-const provider = new ethers.getDefaultProvider('mainnet');
+const { ALCHEMY_API_KEY } = process.env;
+
+const provider = new providers.JsonRpcProvider(ALCHEMY_API_KEY);
 
 const ERC20_ABI = [
     "function name() public view returns (string)",
@@ -9,19 +12,25 @@ const ERC20_ABI = [
     "function totalSupply() public view returns (uint256)"
 ]
 const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // DAI Contract
-const contract = new ethers.Contract(address, ERC20_ABI, provider);
+const contract = new Contract(address, ERC20_ABI, provider);
 
 
 const daiContract = async () => {
-    const symbol = await contract.symbol();
-    const tokenName = await contract.name();
-    const totalSupply = await contract.totalSupply();
-    const Decimals = await contract.decimals();
+    try {
+        const symbol = await contract.symbol();
+        const tokenName = await contract.name();
+        const totalSupply = await contract.totalSupply();
+        const Decimals = await contract.decimals();
 
-    console.log('Symbol:', symbol);
-    console.log('TokenName:', tokenName);
-    console.log('TotalSupply:', totalSupply);
-    console.log('Decimals:', Decimals);
+        console.log('Symbol:', symbol);
+        console.log('TokenName:', tokenName);
+        console.log('TotalSupply:', utils.formatEther(totalSupply));
+        console.log('Decimals:', Decimals);
+    } catch (error) {
+        console.log(error);
+        console.log('Error: Network Error');
+    }
+
 }
 
 daiContract();
